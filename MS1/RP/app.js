8e59311c5e303fceb0821b8b398671bf03136758
DavidJOV <davidjova94@gmail.com>
2018-11-09 21:48:40 +0100

    MS1 RP Abgabe

--- MS1/RP/app.js
+++ MS1/RP/app.js
@@ -8,7 +8,7 @@ cronJob.prüfeMeldungen();
 
 
 // Phantom console.log finden - helper code
-['log', 'warn'].forEach(function(method) {
+/*['log', 'warn'].forEach(function(method) {
     var old = console[method];
     console[method] = function() {
       var stack = (new Error()).stack.split(/\n/);
@@ -19,7 +19,7 @@ cronJob.prüfeMeldungen();
       var args = [].slice.apply(arguments).concat([stack[1].trim()]);
       return old.apply(console, args);
     };
-  });
+  });*/
 
 
 
