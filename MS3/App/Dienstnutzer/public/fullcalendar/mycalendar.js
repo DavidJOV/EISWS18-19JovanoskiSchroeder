c087b7c5acbf972617565187c2218ca8b7d1b5aa
DavidJOV <davidjova94@gmail.com>
2019-01-10 14:15:23 +0100

    Dienstplan Get frontend progress going on...

--- MS3/App/Dienstnutzer/public/fullcalendar/mycalendar.js
+++ MS3/App/Dienstnutzer/public/fullcalendar/mycalendar.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-
+   var url = "http://localhost:3000/Dienstplaene/"+getIndexVonDienstplan();
+   $.get(url,function(dienstplan){
+    console.log(dienstplan)
+   
     // page is now ready, initialize the calendar..
     $('#calendar').fullCalendar({
        
@@ -10,3 +13,19 @@ $(document).ready(function() {
 
 
 });
+})
+
+function getIndexVonDienstplan()
+{
+       var query = window.location.href
+       var vars = query.split("/");
+       for (var i=0;i<vars.length;i++) {
+               if(i+1 == vars.length){
+                   return vars[i];
+                   console.log(vars[i]);
+                }
+              
+               
+       }
+    
+}
